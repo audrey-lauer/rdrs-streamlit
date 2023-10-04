fd6457dd1a21b987bbfe8d2be50cdbf2dd9adc61
Audrey Lauer <audrey.lauer@canada.ca>
2023-10-04 15:30:26 +0000

    Added start of ic406w8 and ic406w9

--- frontend.py
+++ frontend.py
@@ -415,6 +415,50 @@ def make_timeserie(year, clicked_id, clicked_name, clicked_elev, lapse_type, min
         except:
             rdrs_ic406 = False
 
+    rdrs_ic406w8 = False
+    df_rdrs_ic406w8_sd = pd.DataFrame()
+    if 'ic406w8' in version:
+        try:
+            df_rdrs_ic406w8 = pd.read_pickle("data/"+hour_range+"/"+clicked_id+"-RDRSvic406w8.pkl")
+
+            df_rdrs_ic406w8 = df_rdrs_ic406w8.drop_duplicates(subset='date')
+
+            df_rdrs_ic406w8_sd = pd.DataFrame()
+            if sd_or_gradTT in df_rdrs_ic406w8.columns:
+                df_rdrs_ic406w8_sd['date']       = df_rdrs_ic406w8['date']
+                df_rdrs_ic406w8_sd[sd_or_gradTT] = df_rdrs_ic406w8[sd_or_gradTT]
+                mask = (df_rdrs_ic406w8_sd['date'] > date_debut) & (df_rdrs_ic406w8_sd['date'] <= date_fin)
+                df_rdrs_ic406w8_sd = df_rdrs_ic406w8_sd.loc[mask]
+
+            df_rdrs_ic406w8 = find_min_max(df_rdrs_ic406w8, date_list)
+
+            rdrs_ic406w8 = True
+
+        except:
+            rdrs_ic406w8 = False
+
+    rdrs_ic406w9 = False
+    df_rdrs_ic406w9_sd = pd.DataFrame()
+    if 'ic406w9' in version:
+        try:
+            df_rdrs_ic406w9 = pd.read_pickle("data/"+hour_range+"/"+clicked_id+"-RDRSvic406w9.pkl")
+
+            df_rdrs_ic406w9 = df_rdrs_ic406w9.drop_duplicates(subset='date')
+
+            df_rdrs_ic406w9_sd = pd.DataFrame()
+            if sd_or_gradTT in df_rdrs_ic406w9.columns:
+                df_rdrs_ic406w9_sd['date']       = df_rdrs_ic406w9['date']
+                df_rdrs_ic406w9_sd[sd_or_gradTT] = df_rdrs_ic406w9[sd_or_gradTT]
+                mask = (df_rdrs_ic406w9_sd['date'] > date_debut) & (df_rdrs_ic406w9_sd['date'] <= date_fin)
+                df_rdrs_ic406w9_sd = df_rdrs_ic406w9_sd.loc[mask]
+
+            df_rdrs_ic406w9 = find_min_max(df_rdrs_ic406w9, date_list)
+
+            rdrs_ic406w9 = True
+
+        except:
+            rdrs_ic406w9 = False
+
     # RDRS v1
     rdrs_01 = False
     df_rdrs_01_sd = pd.DataFrame()
@@ -575,6 +619,14 @@ def make_timeserie(year, clicked_id, clicked_name, clicked_elev, lapse_type, min
         date = df_rdrs_ic406['date_from'].to_list()
         temp_rdrs_ic406 = np.array(df_rdrs_ic406[min_or_max].to_list())
 
+    if rdrs_ic406w8:
+        date = df_rdrs_ic406w8['date_from'].to_list()
+        temp_rdrs_ic406w8 = np.array(df_rdrs_ic406w8[min_or_max].to_list())
+
+    if rdrs_ic406w9:
+        date = df_rdrs_ic406w9['date_from'].to_list()
+        temp_rdrs_ic406w9 = np.array(df_rdrs_ic406w9[min_or_max].to_list())
+
     if rdrs_01:
         date = df_rdrs_01['date_from'].to_list()
         temp_rdrs_01 = np.array(df_rdrs_01[min_or_max].to_list())
@@ -627,6 +679,14 @@ def make_timeserie(year, clicked_id, clicked_name, clicked_elev, lapse_type, min
     if rdrs_ic406:
         tmax_rdrs_ic406 = ax1.plot(date, (temp_rdrs_ic406 + lapse_rate_rdrs), 'darkviolet', label=min_or_max+' RDRS IC406')
         lns = lns + tmax_rdrs_ic406
+
+    if rdrs_ic406w8:
+        tmax_rdrs_ic406w8 = ax1.plot(date, (temp_rdrs_ic406w8 + lapse_rate_rdrs), 'darkviolet', label=min_or_max+' RDRS IC406w8')
+        lns = lns + tmax_rdrs_ic406w8
+
+    if rdrs_ic406w9:
+        tmax_rdrs_ic406w9 = ax1.plot(date, (temp_rdrs_ic406w9 + lapse_rate_rdrs), 'darkviolet', label=min_or_max+' RDRS IC406w9')
+        lns = lns + tmax_rdrs_ic406w9
 
     if rdrs_ic401:
         tmax_rdrs_ic401 = ax1.plot(date, (temp_rdrs_ic401 + lapse_rate_rdrs), 'magenta', label=min_or_max+' RDRS IC401')
@@ -692,17 +752,23 @@ def make_timeserie(year, clicked_id, clicked_name, clicked_elev, lapse_type, min
         if not df_rdrs_03tdiag_sd.empty:
             sd_rdrs = ax2.plot(df_rdrs_03tdiag_sd['date'],    df_rdrs_03tdiag_sd[sd_or_gradTT], '--', color='gold', label=sd_or_gradTT+' RDRS')
 
+        if not df_rdrs_ic401_sd.empty:
+            sd_rdrs = ax2.plot(df_rdrs_ic401_sd['date'],    df_rdrs_ic401_sd[sd_or_gradTT], '--', color='magenta', label=sd_or_gradTT+' RDRS')
+
+        if not df_rdrs_ic404_sd.empty:
+            sd_rdrs = ax2.plot(df_rdrs_ic404_sd['date'],    df_rdrs_ic404_sd[sd_or_gradTT], '--', color='slateblue', label=sd_or_gradTT+' RDRS')
+
         if not df_rdrs_ic405_sd.empty:
             sd_rdrs = ax2.plot(df_rdrs_ic405_sd['date'],    df_rdrs_ic405_sd[sd_or_gradTT], '--', color='darkviolet', label=sd_or_gradTT+' RDRS')
 
         if not df_rdrs_ic406_sd.empty:
             sd_rdrs = ax2.plot(df_rdrs_ic406_sd['date'],    df_rdrs_ic406_sd[sd_or_gradTT], '--', color='darkviolet', label=sd_or_gradTT+' RDRS')
 
-        if not df_rdrs_ic401_sd.empty:
-            sd_rdrs = ax2.plot(df_rdrs_ic401_sd['date'],    df_rdrs_ic401_sd[sd_or_gradTT], '--', color='magenta', label=sd_or_gradTT+' RDRS')
+        if not df_rdrs_ic406w8_sd.empty:
+            sd_rdrs = ax2.plot(df_rdrs_ic406w8_sd['date'],    df_rdrs_ic406w8_sd[sd_or_gradTT], '--', color='darkviolet', label=sd_or_gradTT+' RDRS')
 
-        if not df_rdrs_ic404_sd.empty:
-            sd_rdrs = ax2.plot(df_rdrs_ic404_sd['date'],    df_rdrs_ic404_sd[sd_or_gradTT], '--', color='slateblue', label=sd_or_gradTT+' RDRS')
+        if not df_rdrs_ic406w9_sd.empty:
+            sd_rdrs = ax2.plot(df_rdrs_ic406w9_sd['date'],    df_rdrs_ic406w9_sd[sd_or_gradTT], '--', color='darkviolet', label=sd_or_gradTT+' RDRS')
 
         if not df_rdrs_01_sd.empty:
             sd_rdrs = ax2.plot(df_rdrs_01_sd['date'],    df_rdrs_01_sd[sd_or_gradTT], '--c', label=sd_or_gradTT+' RDRS')
@@ -762,18 +828,22 @@ if dataset == 'ECCC network' or dataset == 'BC archive' or dataset == 'Wood':
         version_ic404   = st.checkbox('RDRS vIC404')
         version_ic405   = st.checkbox('RDRS vIC405')
         version_ic406   = st.checkbox('RDRS vIC406')
+        version_ic406w8 = st.checkbox('RDRS vIC406w8')
+        version_ic406w9 = st.checkbox('RDRS vIC406w9')
         version_rdps    = st.checkbox('RDPS')
-        version_hrdps    = st.checkbox('HRDPS')
+        version_hrdps   = st.checkbox('HRDPS')
 
         version = []
         if version_02p1: version.append('02P1')
         if version_03test: version.append('3TEST')
         if version_03Lmin: version.append('3Lmin')
         if version_03tdiag: version.append('3tdiaglim')
-        if version_ic405: version.append('ic405')
         if version_ic401: version.append('ic401')
         if version_ic404: version.append('ic404')
+        if version_ic405: version.append('ic405')
         if version_ic406: version.append('ic406')
+        if version_ic406: version.append('ic406w8')
+        if version_ic406: version.append('ic406w9')
         if version_01: version.append('v1')
         if version_rdps: version.append('rdps')
         if version_hrdps: version.append('hrdps')
